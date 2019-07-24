5ef933c1ea39b6b777e8238f734621c1c02c5fa3
Joshua Bracks <joshabracks@gmail.com>
2019-07-23 21:10:21 -0700

    updated models

--- chaos_collab/apps/main_app/models.py
+++ chaos_collab/apps/main_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.core.validators import validate_email
+import bcrypt
 
-# Create your models here.
+# managers
+
+class UserManager(models.Manager):
+    def validate_user(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData['fname']) < 2:
+            errors["fname"] = 'First name should be at least 2 characters'
+        if len(postData['lname']) < 2:
+            errors["lname"] = 'Last name should be at least 2 characters'
+        if len(postData['password']) < 8:
+            errors['password'] = "Password name should be at least 8 characters"
+        if (postData['password']) != (postData['cpassword']):
+            errors['passconfirm'] = "Passwords do not match"
+        if User.objects.filter(email = (postData['email'])):
+            errors['inuse'] = "Email is already in use"
+        try:
+            validate_email(postData['email'])
+        except:
+            errors['invalid'] = "Invalid email"
+        return errors
+    def validate_login(self, postData):
+        user = User.objects.get(email=postData['email'])
+        errors = {}
+        if not bcrypt.checkpw(postData['password'].encode(), user.password.encode()):
+            errors['invalid'] = 'invalid combination'
+        return errors
+
+# models
+
+class User(models.Model):
+    name = models.CharField(max_length=45)
+    screen_name = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
